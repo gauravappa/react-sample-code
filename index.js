b224cf8a209604f985f3bd8e59abc34af8af1fcf
gauravappa <gauravappagga@gmail.com>
2025-03-30 19:58:45 +0530

    resolving conflict first commit

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+<<<<<<< HEAD
+=======
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+>>>>>>> 4cbac44 (npm initialization,parcel and react library installation and related code changes)
 const heading = React.createElement("h1",{style : {color:"red"}},"Ha mai hu Khalnayak");
 
 
