6d51221bf60570c4262b5ce829a938a7f2a7518b
gauravappa <gauravappagga@gmail.com>
2025-04-30 18:00:44 +0530

    JSX react element

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const heading = React.createElement("h1",{style : {color:"red"}},"Ha mai hu Khalnayak");
 
 
-const childDiv = React.createElement("div",{},heading)
-const mainDiv = React.createElement("div",{},childDiv);
+
+
+//Code to create react element using normal javascript syntax
+//const heading = React.createElement("h1",{style : {color:"red"}},"Ha mai hu Khalnayak");
+//const childDiv = React.createElement("div",{},heading)
+//const mainDiv = React.createElement("div",{},childDiv);
+
+//code to create element using jsx syntax
+const heading = <h1 style={{color:"red"}}>ABC</h1>
+const childDiv = <div>{heading}</div>;
+const mainDiv = <div>{childDiv}</div>;
+
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
